4938da6e4f72aae8de7d61708daa439b67377d13
jose-cleiton <cleitons835@gmail.com>
2022-10-27 13:53:27 -0300

    feat: :sparkles: class ProfileConnection

---
+++ social_network/profile.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+
+class ProfileConnections:
+    def __init__(self) -> None:
+        today_date = date.today()
+
+        self._friends = []
+        for friend in friends:
+            friend_data = {"name": friend, "date_affed": today_date}
+            self._friends.append(friend_data)
