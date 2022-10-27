4d32beccc36b1cd8589b82957f871db89cd4bdc1
jose-cleiton <cleitons835@gmail.com>
2022-10-27 16:23:49 -0300

    feat: :sparkles: __next__

--- social_network/profile.py
+++ social_network/profile.py
@@ -9,3 +9,17 @@ class ProfileConnections:
         for friend in friends:
             friend_data = {"name": friend, "date_affed": today_date}
             self._friends.append(friend_data)
+
+    def __iter__(self):
+        return ProfilIterator(self._friends)
+
+
+class ProfilIterator:
+    def __init__(self, friends) -> None:
+        self._cont = 0
+        self.friends = friends
+
+    def __next__(self):
+        next = self.friends[self._cont]
+        self._cont += 1
+        return next
--- tests/test_profile_interator.py
+++ tests/test_profile_interator.py
@@ -7,3 +7,16 @@ def test_can_use_instance_in_loop():
 
     for friend in profile:
         assert friend["name"] in friends
+
+
+def test_can_interate_over_instance():
+    friends = ["Rolf", "Jose", "Charlie"]
+    connection = ProfileConnections(friends)
+
+    con_iter = iter(connection)  # connection.__iter__()
+
+    next_friend = next(con_iter)
+    assert friends[0] == next_friend["name"]
+
+    next_friend = next(con_iter)
+    assert friends[1] == next_friend["name"]
